0a2c2de0a426ffae302759bd56901c8714e45277
shanhan <thecsh3@gmail.com>
2021-07-11 23:07:36 +0800

    Add categories

--- lib/expense.dart
+++ lib/expense.dart
@@ -42,6 +42,10 @@ class ExpenseFormState extends State<ExpenseForm> {
                       )
                   ),
                   sizedBoxSpace,
+                  Text('Expense category'),
+                  sizedBoxSpace,
+                  Categories(),
+                  sizedBoxSpace,
                   Center(
                       child: ElevatedButton(
                           onPressed: () {},
@@ -52,6 +56,79 @@ class ExpenseFormState extends State<ExpenseForm> {
             )
         )
       )
+    );
+  }
+}
+
+class Categories extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return CategoriesState();
+  }
+}
+
+class CategoriesState extends State<Categories> with RestorationMixin {
+  final isSelected = [
+    RestorableBool(false),
+    RestorableBool(false),
+    RestorableBool(false),
+    RestorableBool(false),
+  ];
+
+  @override
+  String get restorationId => 'categories';
+
+  @override
+  void restoreState(RestorationBucket oldBucket, bool initialRestore) {
+    registerForRestoration(isSelected[0], 'item0');
+    registerForRestoration(isSelected[1], 'item1');
+    registerForRestoration(isSelected[2], 'item2');
+    registerForRestoration(isSelected[3], 'item3');
+  }
+
+  @override
+  void dispose(){
+    for (final restorableBool in isSelected){
+      restorableBool.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ToggleButtons(
+      onPressed: (index) {
+        setState(() {
+          isSelected[index].value = !isSelected[index].value;
+        });
+      },
+      isSelected: isSelected.map((element) => element.value).toList(),
+      children: [
+        Column(
+            children: [
+              Icon(Icons.fastfood_outlined),
+              Text('Food & Dining')
+            ]
+        ),
+        Column(
+          children: [
+            Icon(Icons.commute_outlined),
+            Text('Transport')
+          ]
+        ),
+        Column(
+          children: [
+            Icon(Icons.shopping_bag_outlined),
+            Text('Shopping')
+          ]
+        ),
+        Column(
+            children: [
+              Icon(Icons.library_books_outlined),
+              Text('Education')
+            ]
+        )
+      ]
     );
   }
 }
